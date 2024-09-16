cc97f134c93f1cba673a0254f88224cbb0d4571a
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-16 05:53:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-const url = ' https://dog.ceo/api/breeds/image/random'
+const url = ' https://dog.ceo/api/breeds/image/random';
 fetch(url)
-.then(res=>res.json())
-.then(data=>{console.log(data)
-document.getElementById("image-container").innerHTML=`
+	.then((res) => res.json())
+	.then((data) => {
+		console.log(data);
+		document.getElementById('image-container').innerHTML = `
 <img src="${data.message}"/>
-`}
-);
-
-
-
-/**
-Challenge:
-
-1. Fetch a random image from the Dog API again 
-(https://dog.ceo/api/breeds/image/random)
-
-2. Access the DOM and insert the URL you got from the
-API as an image `src` property (probably easiest if 
-you create the image completely here in the JS and add 
-it as the innerHTML of another element on the DOM)
-*/
+`;
+	});
 
