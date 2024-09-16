86b5f2df76af9398083dd75afe96628fc9ef971e
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-16 06:18:01 +0500

    classlist

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 /**
 Challenge: 
 
-- Start building out the BoredBot Skeleton however you'd like. 
-    That will include:
-    - A title for the app ("BoredBot" might be a good start 😉)
-    - A placeholder element that will be populated with the random 
-      idea we get from the API
-    - A button to click for triggering the GET request to the Bored API. 
-      (Don't worry about implementing the button quite yet)
+- Make the styling more exciting once an activity idea comes 
+back from the Bored API
+    - Resources: DOM element "classList" property, uigradients.com, 
+      Google Fonts, color.adobe.com
+    - Some ideas:
+      - Change the title from "BoredBot" to something more exciting!
+      - Change the background to something less drab.
+      - Bonus: Animate something on the screen to move around and add more 
+        excitement to the page
 */
-
-// fetch("https://apis.scrimba.com/bored/api/activity")
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById("activity-name").textContent = data.activity
-//     })
-
-const url = 'https://apis.scrimba.com/bored/api/activity'
-fetch(url)
-.then(res=>res.json())
-.then(data=> {
-  console.log(data);
-  document.querySelector('.activity-text').textContent = data.activity;
-
-});
+const btn=document.getElementById('action-button')
+const url='https://apis.scrimba.com/bored/api/activity'
+function activity(){
+  fetch(url)
+  .then(res=>res.json())
+  .then(data=>{
+    console.log(data);
+    document.getElementById('activity-text').innerText=data.activity
+    document.getElementById('activity-text').classList='subtitle-alt'
+    document.getElementById('bot').innerText='🦾 HappyBot🦿'
+    
+  })
+  
+}
