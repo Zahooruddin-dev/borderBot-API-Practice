68c86d653d04ae08b3664ce3b5665fc37bbec4c2
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-16 06:09:06 +0500

    styling

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-const url = ' https://dog.ceo/api/breeds/image/random';
-fetch(url)
-	.then((res) => res.json())
-	.then((data) => {
-		console.log(data);
-		document.getElementById('image-container').innerHTML = `
-<img src="${data.message}"/>
-`;
-	});
+/**
+Challenge: 
 
+- Start building out the BoredBot Skeleton however you'd like. 
+    That will include:
+    - A title for the app ("BoredBot" might be a good start 😉)
+    - A placeholder element that will be populated with the random 
+      idea we get from the API
+    - A button to click for triggering the GET request to the Bored API. 
+      (Don't worry about implementing the button quite yet)
+*/
+
+// fetch("https://apis.scrimba.com/bored/api/activity")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//         document.getElementById("activity-name").textContent = data.activity
+//     })
+
+const url = 'https://apis.scrimba.com/bored/api/activity'
+fetch(url)
+.then(res=>res.json())
+.then(data=> {
+  console.log(data);
+  document.querySelector('.activity-text').textContent = data.activity;
+
+});
